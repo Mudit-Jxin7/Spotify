fba50c03940e576d07fc002c46ed6c27bc78b4bf
Mudit <muditert34@gmail.com>
2023-07-10 12:03:24 +0530

    player functionality

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import ToasterProvider from "@/providers/ToasterProvider";
 import { Figtree } from "next/font/google";
 import Sidebar from "@/components/Sidebar";
 import getSongsByUserId from "@/actions/getSongsByUserId";
+import Player from "@/components/Player";
 
 const font = Figtree({ subsets: ["latin"] });
 
@@ -31,6 +32,7 @@ export default async function RootLayout({
           <UserProvider>
             <ModalProvider />
             <Sidebar songs={userSongs}>{children}</Sidebar>
+            <Player />
           </UserProvider>
         </SupabaseProvider>
       </body>
